082c6662b17f72bdcae63705e221ff58046f7756
a1exsh <alex.shulgin@gmail.com>
2023-12-29 20:20:57 +0100

    update deps

--- dev/user.clj
+++ dev/user.clj
@@ -6,15 +6,18 @@
   (clerk/serve! {:browse? true :port 6677})
 
   ;; start with file watcher for these sub-directory paths
-  (clerk/serve! {:watch-paths ["notebooks"]})
+  (clerk/serve! {:watch-paths ["notebooks"] :port 6677})
 
   ;; start with file watcher and a `show-filter-fn` function to watch
   ;; a subset of notebooks
   (clerk/serve! {:watch-paths ["notebooks" "src"] :show-filter-fn #(clojure.string/starts-with? % "notebooks")})
 
   (clerk/clear-cache!)
+  ;; stop
+  (clerk/halt!)
 
   ;; or call `clerk/show!` explicitly
+  (clerk/show! "notebooks/macros.clj")
   (clerk/show! "notebooks/four_twos.clj")
   (clerk/show! "notebooks/literal_compare.clj")
 
