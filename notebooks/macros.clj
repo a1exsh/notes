a8a3c3c3e30492b74dad3a57196049335eb9e56e
a1exsh <alex.shulgin@gmail.com>
2023-12-31 16:48:15 +0100

    more macros

--- notebooks/macros.clj
+++ notebooks/macros.clj
@@ -18,14 +18,14 @@
 (with-out-str
   (with-debug 1))
 
-;; Prints the `body` form, followed by `=>` and the result of `body`
-;; evaluation.  Returns that result.
+;; Prints the `form`, followed by `=>` and the result of its evaluation.
+;; Returns that result.
 ;;
-;; Note the quoting of the unquote-spliced body...
-(defmacro with-trace [& body]
+;; Note the quoting of the unquoted form...
+(defmacro with-trace [form]
   `(do
-     (print '~@body "=> ")
-     (let [res# ~@body]
+     (print '~form "=> ")
+     (let [res# ~form]
        (println res#)
        res#)))
 
@@ -33,3 +33,39 @@
 
 (with-out-str 
   (with-trace (+ 1 2 3)))
+
+;; ## Doing the work in fns
+(defn str-form [form]
+  `(str ~form))
+
+(str-form 1)
+
+(defn str-forms [& forms]
+  (map str-form forms))
+
+(str-forms 'a 'b 'c)
+
+(defmacro stringify [& forms]
+  ;; without the outer `(str)` it will look like a function call on a
+  ;; `String`, which doesn't work for obvious reason
+  `(str ~@(apply str-forms forms)))
+
+(macroexpand-1
+ '(stringify a b c))
+
+(stringify 'a 'b 'c)
+
+;; It also works directly, so no great benefit of having `str-forms` fn in
+;; this simple case:
+(defmacro stringify-direct [& forms]
+  `(str ~@(map str-form forms)))
+
+(stringify-direct 'a 'b 'c)
+
+;; Or completely directly:
+(defmacro stringify-most-direct [& forms]
+  `(str ~@(->> forms
+               (map (fn [form]
+                      `(str ~form))))))
+
+(stringify-most-direct 'a 'b 'c)
